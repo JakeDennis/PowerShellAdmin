47946eb6f71593b04124d258590d427d591c331d
Jake Dennis <JakeJDennis@gmail.com>
2019-05-02 17:47:54 -0500

    Add files via upload

---
+++ ComparePatches.ps1
@@ -0,0 +1,11 @@
+﻿$server1 = Read-Host "Server 1"
+$server2 = Read-Host "Server 2"
+$creds = Get-Credential 
+
+$server1Patches = get-hotfix -computer $server1 -Credential $creds | Where-Object {$_.HotFixID -ne "File 1"}
+
+$server2Patches = get-hotfix -computer $server2 -Credential $creds | Where-Object {$_.HotFixID -ne "File 1"}
+
+$m = Compare-Object ($server1Patches) ($server2Patches) -Property HotFixID | measure
+
+$m.Count
