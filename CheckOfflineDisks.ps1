b0c7f93c1f21b69c24ce4fe6490925f6e10b3c8d
Jake Dennis <JakeJDennis@gmail.com>
2019-05-02 17:53:53 -0500

    Add files via upload

---
+++ CheckOfflineDisks.ps1
@@ -0,0 +1,100 @@
+﻿<#
+ NAME: Get-OfflineDisks.ps1
+ AUTHOR: Jake Dennis
+ DATE  : 8/16/2018
+
+ DESCRIPTION
+     This script will check if disks on a server are offline using diskpart.exe. If offline, an automated attempt to online the disk will be executed using DiskPart.exe
+ EXAMPLE
+     PS C:\Users\user1> Get-OfflineDisks | Out-string
+        Offline disk(s) found:
+          Disk 1    Offline          40 GB  1024 KB         
+
+        Attempting to online the disk(s)...
+        Attempt to online the disk(s) suceeded.
+
+        Online Disk(s):
+          Disk 0    Online           60 GB      0 B            Disk 1    Online           40 GB  1024 KB         
+
+        Microsoft DiskPart version 6.3.9600
+
+        Copyright (C) 1999-2013 Microsoft Corporation.
+        On computer: SERVER1
+#>
+
+#Start Function
+Function Get-OfflineDisks{
+    #Check for offline disks on the server and set variables for disk status
+    $OnlineDisks = "List Disk" | diskpart | where {$_ -match "online"}
+    $OfflineDisks = "List Disk" | diskpart | where {$_ -match "offline"}
+
+    #If offline disk(s) exist
+    if($OfflineDisks)
+    {
+        Write-Host "Offline disk(s) found:"
+        Write-Host $OfflineDisks
+        Write-Host ""
+        Write-Host "Attempting to online the disk(s)..."
+        Write-Host ""
+        #for all offline disk(s) found on the server
+        foreach($OffDisk in $OfflineDisks)
+        {
+    
+            $OffDisks = $OffDisk.Substring(2,6)
+
+#Creating command parameters for selecting the disk in DiskPart, attempting to online disk, and disabling read-only attribute
+$AutoOnlineDisk = @"
+select $OffDisks
+attributes disk clear readonly
+online disk
+attributes disk clear readonly
+"@
+            #Sending parameters to diskpart
+            $AutoOnlineDisk | diskpart
+        }
+        #Scan disks again after auto-fix
+        $OfflineDisks = "List Disk" | diskpart | where {$_ -match "offline"}
+        $OnlineDisks = "List Disk" | diskpart | where {$_ -match "online"}
+
+        #If auto-fix failed
+        if($OfflineDisks)
+        {
+            Write-Host "Attempt to online the disk(s) failed."
+            Write-Host "Offline disk(s) found:"
+            Write-Host $OfflineDisks
+            Write-Host ""
+            Write-Host $OnlineDisks
+        }
+        
+        #If auto-fix succeeded
+        else
+        {
+            Write-Host "Attempt to online the disk(s) suceeded."
+            Write-Host ""
+            Write-Host "Online Disk(s):"
+            Write-Host $OnlineDisks
+        }
+
+    }
+    #If no offline disk(s) existed
+        else
+        {
+            Write-Host "All disk(s) are online."
+            Write-Host $OnlineDisks
+        }
+}
+
+#Output function to string variable for email body
+$EmailBody = Get-OfflineDisks | Out-String
+
+<#Set email parameters
+$Email = @{
+From = "Dev Refresh Disk Check <Sender@domain.com>"
+To = @("Recipient@domain.com")
+Subject = "Disk Check Report"
+SMTPServer = "smtp@domain.com"
+Body = $EmailBody
+}
+#>
+#Send email; Must be sent as plain text in current format
+#Send-MailMessage @Email
---
+++ NTPTimeDriftMonitor.ps1
@@ -0,0 +1,69 @@
+﻿<#
+ NAME: NTP Time and Drift Monitor.ps1
+ 
+ AUTHOR: Jake Dennis
+ DATE  : 3/1/2018
+
+ DESCRIPTION
+     Script used to monitor NTP servers for latency and time drift from their source and import into SolarWinds SAM module.
+     This version differs from the version SolarWinds uses due to output requirements and 32-bit constraints. 
+     This version is designed for 64-bit PowerShell and outputs values with labels
+#>
+
+### Get Date and Time for Logging
+function Get-TimeStamp {
+    
+   return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+    
+}
+
+### Get SWIS-Snapin for Database Connection and CRUD commands
+if (-not (Get-PSSnapin | where {$_.Name -eq "SwisSnapin"})) {
+
+    Add-PSSnapin "SwisSnapin"
+
+} 
+
+$swis = Connect-Swis –Trusted –Hostname localhost
+
+#Run the script with the NTP Server hostname as the argument (e.g. PS D:\> & 'D:\Scripts\NTP Time and Drift Monitor.ps1' antp)
+
+$NTPServer = $args[0];
+$CurrentTime = w32tm /monitor /computers:$NTPserver
+
+#Error Check and exit with 1 value for SolarWinds to interpret as down
+if($CurrentTime -match "Error")
+{
+    Write-Host Message:NTP request to $NTPServer failed to query for time statistics
+
+    Exit 1
+}
+else
+{
+    #Pulling Array of Characters out of w32tm command output
+    $Stratum = $CurrentTime | Select-String -Pattern "Stratum:" #-Context 9
+    $Latency = $CurrentTime | Select-String -Pattern "ICMP:" #-Context 35,15
+    $Source = $CurrentTime | Select-String -Pattern "RefID:" #-Context 9,20
+    $Drift = $CurrentTime | Select-String -Pattern "NTP:" #-Context 10,5
+                
+    #Turning array object types into strings
+    $StratumShort = [string]$Stratum
+    $LatencyShort = [string]$Latency
+    $SourceShort = [string]$Source
+    $DriftShort = [string]$Drift
+
+    #Trim strings to only numbers for statistic ingest by SolarWinds
+    $SourceShort = $SourceShort.Substring(35)
+    $StratumShort = $StratumShort.Split(@(" "))
+    $StratumShort = $StratumShort[1]
+    $LatencyShort = $LatencyShort.Substring(10)
+    $DriftShort = $DriftShort.Substring(9,20)
+    
+    #Output variables in the correct format of either message or statistic for SolarWinds ingest
+    Write-Host Message.Source: $SourceShort
+    Write-Host Statistic.Stratum: $StratumShort
+    Write-Host Statistic.Drift: $DriftShort
+    Write-Host Statistic.Latency: $LatencyShort
+
+    Exit 0
+}
